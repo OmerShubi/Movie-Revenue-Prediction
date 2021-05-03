fix normalization and dummies

preprocessing.py
@@ -7,11 +7,15 @@ from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy import stats
-from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, OneHotEncoder
 import os
 import pickle
 from datetime import datetime
 from sklearn.pipeline import Pipeline
+import joblib
+from sklearn.compose import ColumnTransformer, make_column_transformer
+
+
 
 
 def treat_dict_column(data, old_col_name, new_col_name, key):
@@ -60,13 +64,19 @@ def parse_data(data, max_order=2, train=True):
     # Convert Bool to 1 and 0
     data['video'] = data['video'].astype(int)
 
+    # Convert release_date to timestamp
+    data['release_date'] = data['release_date'].apply(lambda x: datetime.timestamp(x))
+
     numerical_columns = ["popularity", "budget", "runtime", "vote_average", "vote_count"]
-    pipe = Pipeline([('standard_scaler', StandardScaler()), ('minmax_scaler', MinMaxScaler())], memory='scaler')
+    dummy_columns = ["collection_name", "original_language"]
+    for i in range(max_order):
+        dummy_columns.append(f'cast_{i}_name')
 
     if train:
-        # TODO check if working
         # Normalized numerical features
-        data[numerical_columns] = pipe.fit_transform(data[numerical_columns].to_numpy())
+        pipe = Pipeline([('standard_scaler', StandardScaler()), ('minmax_scaler', MinMaxScaler())])
+        pipe.fit(data[numerical_columns].to_numpy())
+        joblib.dump(pipe, 'pipeline_scaler.pkl')
 
         # # TODO - change to on hot of sklearn & save it in train and use in test
         # # Create dummy variables
@@ -75,28 +85,62 @@ def parse_data(data, max_order=2, train=True):
         #     data = data.join(data[col].str.join(sep='*').str.get_dummies(sep='*').add_prefix(f"{col}_")).drop(col,
         #                                                                                                       inplace=False,
         #                                                                                                       axis=1)
+        encoders = {}
+        for col in dummy_columns:
+            enc = OneHotEncoder(handle_unknown='ignore')
+            enc.fit(data[col].to_numpy().reshape(-1, 1))
+            encoders[col] = enc
+        joblib.dump(encoders, 'encoders.pkl')
+        # data = pd.get_dummies(data, columns=dummy_columns, dummy_na=True)
+        # preprocess = make_column_transformer((["collection_name", "original_language"], OneHotEncoder()))
+        # preprocess.fit_transform(data[["collection_name", "original_language"]]).toarray()
+
+        # encoders = OneHotEncoder(handle_unknown='ignore')
+        # encoders.fit(data['collection_name'].unique().reshape(1, -1))
+        # transformed = encoders.transform(data["collection_name"].to_numpy().reshape(-1, 1))
+        # # Create a Pandas DataFrame of the hot encoded column
+        # ohe_df = pd.DataFrame(transformed, columns=encoders.get_feature_names())
+        # # concat with original data
+        # data = pd.concat([data, ohe_df], axis=1).drop(["collection_name"], axis=1)
+
         # dummy_columns = ["collection_name", "original_language"]
         # for i in range(max_order):
         #     dummy_columns.append(f'cast_{i}_name')
-        # data = pd.get_dummies(data, columns=dummy_columns, dummy_na=True)
+        # #for col in dummy_columns:
+        # encoders.fit(data["collection_name"])
+        # for col in dummy_columns:
+        #     transformed = encoders.transform(data["collection_name"].to_numpy().reshape(-1, 1))
+        #     # Create a Pandas DataFrame of the hot encoded column
+        #     ohe_df = pd.DataFrame(transformed, columns=encoders.get_feature_names())
+        #     # concat with original data
+        #     data = pd.concat([data, ohe_df], axis=1).drop(["collection_name"], axis=1)
+        #     #pd.get_dummies(data, columns=dummy_columns, dummy_na=True)
+        #     #encoders.fit(X)
+        #     # TODO dummy_na=True
     else:
-        # TODO use pretrained scaler & dummy variables
-        data[numerical_columns] = pipe.transform(data[numerical_columns].to_numpy())
-        pass
+        # TODO use pretrained dummy variables
+        # TODO check if dummy is not in the train
+        pipe = joblib.load('pipeline_scaler.pkl')
+        encoders = joblib.load('encoders.pkl')
+    data[numerical_columns] = pipe.transform(data[numerical_columns].to_numpy())
+    data_arr_dummies = []
+    for col in dummy_columns:
+        enc = encoders[col]
+        data_arr_dummies.append(enc.transform(data[col].to_numpy().reshape(-1, 1)).toarray())
 
-    # Convert release_date to timestamp
-    data['release_date'] = data['release_date'].apply(lambda x: datetime.timestamp(x))
+    data_arr = np.concatenate([data.to_numpy()]+data_arr_dummies, axis=1)
 
-    return data
+    return data_arr
 
 if __name__ == '__main__':
+    train = True
     train_path = 'train.tsv'
     train_data = pd.read_csv(train_path, sep="\t", index_col='id', parse_dates=['release_date'])
     parsed_train_path = "parsed_train_data.pkl"
     if os.path.exists(parsed_train_path):
         parsed_train_data = pd.read_pickle(parsed_train_path)
     else:
-        parsed_train_data = parse_data(train_data, train=False)
+        parsed_train_data = parse_data(train_data, train=train)
         # TODO uncomment
         # pd.to_pickle(parsed_train_data, parsed_train_path)
 